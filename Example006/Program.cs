fbd23c2989ae3c008482d7ca3f2d328d916e4e88
Димасик <wadmin@RVS.ru>
2023-08-06 19:41:47 +1000

    Пример 6 с Машей

---
+++ Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Cosole.Readline();
+
+if (username.ToLower() == "Маша");
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username); 
+}
